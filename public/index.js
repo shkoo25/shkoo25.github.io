7c7774796ae30fcbe4b3496f19f581921522a745
Steve Koo <Steve@Steves-MacBook-Pro.local>
2016-01-29 10:55:41 -0500

    Broken slick.css, trying to fix it

--- public/index.js
+++ public/index.js
@@ -1,19 +1,27 @@
 
 $(document).on("ready", function(){
 
-  $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
+  $('.project-container').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1
   });
-});
+
+
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 })
